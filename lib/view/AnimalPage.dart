8f809ba1af4387817573062f4ca03fb5b4c148ff
Mihael-Scofield <mihaelscofield@gmail.com>
2020-03-20 16:59:12 -0300

    Navegação condicional pronta

--- lib/view/AnimalPage.dart
+++ lib/view/AnimalPage.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
 
 class AnimalPage extends StatefulWidget {
+  String chosenAnimal;
+
+  AnimalPage(this.chosenAnimal);
+  
   @override
   State<StatefulWidget> createState() {
-    return AnimalPageState();
+    return AnimalPageState(this.chosenAnimal);
   }
 }
 
 class AnimalPageState extends State<AnimalPage> {
+  String chosenAnimal;
+  
+  AnimalPageState(this.chosenAnimal);
+  String titlePage;
+
+  String decideTitlePage() {
+    if (chosenAnimal == 'cachorro') {
+      return "Cachorros amam abanar o rabo";
+    }
+    else {
+      return "Gatos amam morder os donos";
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    titlePage = decideTitlePage();
+  }  
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Cachorros amam abanar o rabo!"),
+        title: Text(titlePage),
       ),
     
     body: Center(
